e31a42e123981bb10ae4a5dba7cd22c0457c1935
ArvanKumarPaul <71247101+ArvanKumarPaul@users.noreply.github.com>
2022-01-09 17:57:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+img = "";
+noseX = "";
+noseY = "";
+MarioX = "325";
+MarioY = "325";
+
 function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
@@ -5,12 +11,63 @@ function preload() {
 }
 
 function setup() {
+
+	video = createCapture(VIDEO);
+	video.size(600,400);
 	canvas = createCanvas(1240,336);
+	canvas.parent('#myCanvas')
 	instializeInSetup(mario);
+	poseNet = ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+
 }
 
 function draw() {
-	game()
+
+	game();
+
+	if(noseX > 300) {
+
+      MarioX = MarioX + 1;
+
+	}
+
+	image(img,MarioX,MarioY,30,60);
+
+	if(noseX < 300) {
+
+		MarioX = MarioX - 1;
+  
+	}
+
+    image(img,MarioX,MarioY,30,60);
+
+	if(noseY < 150) {
+
+		MarioY = MarioY - 1;
+  
+	}
+
+    image(img,MarioX,MarioY,30,60);
+  
+}
+
+function modelLoaded() {
+
+  console.log("Model Loaded!");
+
+}
+
+function gotPoses(results) {
+
+  if(results.length>0) {
+
+    noseX = results[0].pose.nose.x;
+	noseY = results[0].pose.nose.y;
+	console.log("Nose X = "+noseX+", Nose Y = "+noseY);
+
+  }
+
 }
 
 
